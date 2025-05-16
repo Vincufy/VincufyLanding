a264057228c7b0c392b1d2cd4481817cd9a6f483
zineR1 <agusdalvit@gmail.com>
2025-05-16 14:55:54 -0300

    fix(change base)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ← si usás dominio personalizado, dejalo como '/'
+  base: "/VincufyLanding", // ← si usás dominio personalizado, dejalo como '/'
 });
