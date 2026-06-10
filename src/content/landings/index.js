6b9a1f1eb63fe9825b6bf2ba18ea2a9bb2dbaa48
Daniel Vinet <daniel1vinet@gmail.com>
2026-06-10 08:01:19 -0300

    feat(content): segment 'casual' renderiza landing de productores (URL se mantiene /r/casual)

--- src/content/landings/index.js
+++ src/content/landings/index.js
@@ -1,9 +1,8 @@
 import { boliches } from "./boliches.js";
 import { productores } from "./productores.js";
-import { casual } from "./casual.js";
 
 export const landingsBySegment = {
   boliches,
   productores,
-  casual,
+  casual: productores,
 };
